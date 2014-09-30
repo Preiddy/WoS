2cb6f6e051955f01a1f613e633c07bf5cf578b76
Preiddy <preiddy@gmail.com>
2014-09-30 08:28:22 +0200

    Update bibliometrics.r
    Add BRADFORD

--- R/bibliometrics.r
+++ R/bibliometrics.r
@@ -21,7 +21,12 @@ lang=dbGetQuery(mycon,"SELECT LA,  COUNT(LA) AS Frec FROM isi_t_todo GROUP BY LA
 prod.au=dbGetQuery(mycon,"SELECT AU,  COUNT(AU) AS Frec FROM isi_t_autor GROUP BY TRIM(AU) ORDER BY Frec DESC")
 #Category
 prod.cat=dbGetQuery(mycon,"SELECT WC,  COUNT(WC) AS Frec FROM isi_t_cat GROUP BY TRIM(WC) ORDER BY Frec DESC")
-
+# BRADFORD
+brad <- dbGetQuery(mycon,"SELECT COUNT(*) AS NumRev, cnt AS NumTrabajos 
+FROM (SELECT J9, COUNT(*) AS 'cnt' FROM isi_t_todo GROUP BY J9 ORDER BY cnt DESC) 
+GROUP BY NumTrabajos ORDER BY NumRev, NumTrabajos DESC")
+brad.dist<-cbind(brad,TotArt=brad$NumRev*brad$NumTrabajos, AcumRev=cumsum(brad$NumRev),AcumArt=cumsum(brad$NumRev*brad$NumTrabajos))
+plot(brad.dist$AcumRev,brad.dist$AcumArt,log="x",type="l", col="red", xlab="Log. Acum.Revistas",ylab="Acum. Articulos", lwd=5)
 #Close
 sqliteCloseConnection(mycon)
 sqliteCloseDriver(driver)
